687f13e2d1b869598c656a9f0740d2cebd61bf32
Parth Srivastava <60739970+savss624@users.noreply.github.com>
2021-10-18 14:39:05 +0530

    config updation

---
+++ web/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNSSfK0ITGf6ntvCscimeuRwzp1PqftQg",
+  authDomain: "posnauseam.firebaseapp.com",
+  databaseURL: "https://posnauseam-default-rtdb.firebaseio.com",
+  projectId: "posnauseam",
+  storageBucket: "posnauseam.appspot.com",
+  messagingSenderId: "319175246115",
+  appId: "1:319175246115:web:db63ae4f0e481a7c0a8c96"
+};
+
+firebase.initializeApp(firebaseConfig);
